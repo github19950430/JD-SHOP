0bfe9ac4cb108287b4cb1c781c90c4167f64f0ef
github19950430 <44158178+github19950430@users.noreply.github.com>
2019-01-05 08:49:06 +0800

    测试提交

---
+++ jd-consumer-judiciary/src/main/java/com/example/jdconsumerjudiciary/pay/alipay/PayupPrice.java
@@ -0,0 +1,53 @@
+package com.example.jdconsumerjudiciary.pay.alipay;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.example.jdconsumerjudiciary.pay.payutil.AlipayConfig;
+
+public class PayupPrice {
+
+    public String pay(String WIDout_trade_no,String WIDtotal_amount,String WIDsubject,Integer WIDjudID,Integer WIDuserid){
+        //获得初始化的AlipayClient
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfig.gatewayUrl, AlipayConfig.app_id, AlipayConfig.merchant_private_key, "json", AlipayConfig.charset, AlipayConfig.alipay_public_key, AlipayConfig.sign_type);
+
+        //设置请求参数
+        AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();
+        alipayRequest.setReturnUrl(AlipayConfig.return_urlupprice);
+        alipayRequest.setNotifyUrl(AlipayConfig.notify_url);
+        //+ "?WIDjudID=" + WIDjudID + "&WIDuserid=" + WIDuserid
+        /*//商户订单号，商户网站订单系统中唯一订单号，必填
+        String out_trade_no = new String(request.getParameter("WIDout_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+        //付款金额，必填
+        String total_amount = new String(request.getParameter("WIDtotal_amount").getBytes("ISO-8859-1"),"UTF-8");
+        //订单名称，必填
+        String subject = new String(request.getParameter("WIDsubject").getBytes("ISO-8859-1"),"UTF-8");
+        //商品描述，可空
+        String body = new String(request.getParameter("WIDbody").getBytes("ISO-8859-1"),"UTF-8");*/
+
+        alipayRequest.setBizContent("{\"out_trade_no\":\""+ WIDout_trade_no +"\","
+                + "\"total_amount\":\""+ WIDtotal_amount +"\","
+                + "\"subject\":\""+ WIDsubject +"\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+
+        //若想给BizContent增加其他可选请求参数，以增加自定义超时时间参数timeout_express来举例说明
+        //alipayRequest.setBizContent("{\"out_trade_no\":\""+ out_trade_no +"\","
+        //		+ "\"total_amount\":\""+ total_amount +"\","
+        //		+ "\"subject\":\""+ subject +"\","
+        //		+ "\"body\":\""+ body +"\","
+        //		+ "\"timeout_express\":\"10m\","
+        //		+ "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+        //请求参数可查阅【电脑网站支付的API文档-alipay.trade.page.pay-请求参数】章节
+
+        //请求
+        String result = null;
+        try {
+            result = alipayClient.pageExecute(alipayRequest).getBody();
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
