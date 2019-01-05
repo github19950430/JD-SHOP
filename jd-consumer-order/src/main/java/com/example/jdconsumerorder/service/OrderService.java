3741b475f5aec307393415409e689804a09486c9
github19950430 <44158178+github19950430@users.noreply.github.com>
2019-01-05 09:00:02 +0800

    测试提交

--- jd-consumer-order/src/main/java/com/example/jdconsumerorder/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.example.jdconsumerorder.service;
-
-import com.example.jdproducerorder.entity.solr.OrderVo;
-
-public interface OrderService {
-
-    String createOrder(OrderVo orderVo);
-}
--- jd-producer-order/src/main/java/com/example/jdproducerorder/config/SchedledConfiguration.java
+++
@@ -1,6 +0,0 @@
-package com.example.jdproducerorder.config;
-
-
-public class SchedledConfiguration {
-
-}
